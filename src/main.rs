45a482228c43434070e4882975c53e3a95d09992
Tamás Tóth <tomjtoth@gmail.com>
2025-09-06 10:04:43 +0300

    main.rs to render all components + main.css removed

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,14 @@
 use dioxus::prelude::*;
 
+use crate::components::{
+    context_provider::{child::Child, parent::Parent},
+    controlled_input::ControlledInput,
+    counter::Counter,
+};
+
+mod components;
+
 const FAVICON: Asset = asset!("/assets/favicon.ico");
-const MAIN_CSS: Asset = asset!("/assets/main.css");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
 
 fn main() {
@@ -12,7 +19,30 @@ fn main() {
 fn App() -> Element {
     rsx! {
         document::Link { rel: "icon", href: FAVICON }
-        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
-        Hero {}
+        document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+
+        div {
+            class: "flex flex-col p-2 gap-2 items-center",
+
+            Counter {}
+
+            ControlledInput {}
+
+            hr {}
+
+            // DEMO: context_providers
+            Parent { bg: "bg-red-200",
+                Child { bg: "bg-purple-200" }
+                Child { bg: "bg-green-200" }
+            }
+
+            hr {}
+
+            Parent { bg: "bg-gray-200",
+                Child { bg: "bg-blue-200" }
+                Child { bg: "bg-yellow-200" }
+            }
+        }
+
     }
 }
