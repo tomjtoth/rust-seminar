9b96e1154eae29d3e29727aaecf4b7c8a835dc1a
Tamás Tóth <tomjtoth@gmail.com>
2025-09-06 13:23:44 +0300

    impl fn ContextProvider for demonstrating parent/child

--- src/components/context_provider/mod.rs
+++ src/components/context_provider/mod.rs
@@ -1,2 +1,22 @@
+use dioxus::prelude::*;
+
 pub mod child;
 pub mod parent;
+
+use child::Child;
+use parent::Parent;
+
+#[component]
+pub fn ContextProvider() -> Element {
+    rsx! {
+        Parent { bg: "bg-red-200",
+            Child { bg: "bg-purple-200" }
+            Child { bg: "bg-green-200" }
+        }
+
+        Parent { bg: "bg-gray-200",
+            Child { bg: "bg-blue-200" }
+            Child { bg: "bg-yellow-200" }
+        }
+    }
+}
