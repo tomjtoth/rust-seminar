311063338df808e74bd10db05dcd663954397c93
Tamás Tóth <tomjtoth@gmail.com>
2025-09-06 13:24:22 +0300

    impl fn NavBar

---
+++ src/components/navbar.rs
@@ -0,0 +1,19 @@
+use dioxus::prelude::*;
+
+use crate::routes::Route;
+
+#[component]
+pub fn NavBar() -> Element {
+    rsx! {
+        nav {
+            ul {
+                class: "p-2 flex gap-2 *:border *:rounded *:p-2",
+                li { Link {to: Route::Counter {}, "counter"} }
+                li { Link {to: Route::ControlledInput {}, "controlled input"} }
+                li { Link {to: Route::ContextProvider {}, "context providers"} }
+            }
+        }
+
+        Outlet::<Route> {}
+    }
+}
