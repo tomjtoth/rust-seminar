90d66af750be38afe09f380256c01d9c65a41b27
Tamás Tóth <tomjtoth@gmail.com>
2025-09-06 13:24:54 +0300

    migrated main.rs to use the router layout

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 use dioxus::prelude::*;
 
-use crate::components::{
-    context_provider::{child::Child, parent::Parent},
-    controlled_input::ControlledInput,
-    counter::Counter,
-};
-
 mod components;
+mod routes;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
@@ -21,28 +16,6 @@ fn App() -> Element {
         document::Link { rel: "icon", href: FAVICON }
         document::Link { rel: "stylesheet", href: TAILWIND_CSS }
 
-        div {
-            class: "flex flex-col p-2 gap-2 items-center",
-
-            Counter {}
-
-            ControlledInput {}
-
-            hr {}
-
-            // DEMO: context_providers
-            Parent { bg: "bg-red-200",
-                Child { bg: "bg-purple-200" }
-                Child { bg: "bg-green-200" }
-            }
-
-            hr {}
-
-            Parent { bg: "bg-gray-200",
-                Child { bg: "bg-blue-200" }
-                Child { bg: "bg-yellow-200" }
-            }
-        }
-
+        Router::<routes::Route> {}
     }
 }
