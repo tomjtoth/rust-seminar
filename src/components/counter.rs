0a5254ca1933f1f8ab04e0d0ccdba604b1f89741
Tamás Tóth <tomjtoth@gmail.com>
2025-09-06 13:22:21 +0300

    rm unnecessary div from counter

--- src/components/counter.rs
+++ src/components/counter.rs
@@ -5,16 +5,14 @@ pub fn Counter() -> Element {
     let mut counter = use_signal(|| 0);
 
     rsx! {
-        div {
-            "current count: "
-            button {
-                class: "w-fit",
-                title: "increment by one",
-                onclick: move |_evt| {
-                    counter.with_mut(|w| *w += 1)
-                },
-                "{counter}++"
-            }
+        "current count: "
+        button {
+            class: "w-fit",
+            title: "increment by one",
+            onclick: move |_evt| {
+                counter.with_mut(|w| *w += 1)
+            },
+            "{counter}++"
         }
     }
 }
