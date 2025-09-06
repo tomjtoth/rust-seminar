2936353778b8c86a90b5ea69b2aefc871433e13b
Tamás Tóth <tomjtoth@gmail.com>
2025-09-06 14:15:09 +0300

    cascading p-2 to all children of nav>ul

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -7,7 +7,7 @@ pub fn NavBar() -> Element {
     rsx! {
         nav {
             ul {
-                class: "p-2 flex gap-2 *:border *:rounded *:p-2",
+                class: "p-2 flex gap-2 *:border *:rounded **:p-2",
                 li { Link {to: Route::Counter {}, "counter"} }
                 li { Link {to: Route::ControlledInput {}, "controlled input"} }
                 li { Link {to: Route::ContextProvider {}, "context providers"} }
