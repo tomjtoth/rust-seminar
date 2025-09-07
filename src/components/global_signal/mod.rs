2dffa643b94738161b6cc7cc1bc41cb28fb82756
Tamás Tóth <tomjtoth@gmail.com>
2025-09-07 18:52:50 +0300

    refined incremental steps in global-signals

--- src/components/global_signal/mod.rs
+++ src/components/global_signal/mod.rs
@@ -8,9 +8,16 @@ mod model;
 #[component]
 pub fn FnGlobalSignal() -> Element {
     rsx! {
-        Incrementer { increment_by: 1 }
+        Incrementer { increment_by: 20 }
+        Incrementer { increment_by: 10 }
         Incrementer { increment_by: 5 }
-        Incrementer { increment_by: -10 as i8 }
+        Incrementer { increment_by: 1 }
+
         Nullifier {}
+
+        Incrementer { increment_by: -1 as i8 }
+        Incrementer { increment_by: -5 as i8 }
+        Incrementer { increment_by: -10 as i8 }
+        Incrementer { increment_by: -20 as i8 }
     }
 }
