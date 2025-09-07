e91d94d5c4972859cdf10d13b4c9a81fdc7df48b
Tamás Tóth <tomjtoth@gmail.com>
2025-09-07 20:03:33 +0300

    protect agains overflow of u8

--- src/components/callback/handler.rs
+++ src/components/callback/handler.rs
@@ -4,7 +4,12 @@ pub(super) async fn use_handler(callback: Callback<u8>) {
     let mut idx = use_signal(|| 0);
 
     callback.call(idx.with_mut(|w| {
-        *w += 1;
+        *w = if *w as u16 + 1 > u8::MAX as u16 {
+            0
+        } else {
+            *w + 1
+        };
+
         *w
     }))
 }
