b69f91772dda570d7ed3ace5761e3869b87be581
Tamás Tóth <tomjtoth@gmail.com>
2025-09-08 10:33:06 +0300

    use checked_add in callback/handler

--- src/components/callback/handler.rs
+++ src/components/callback/handler.rs
@@ -1,16 +1,13 @@
 use dioxus::prelude::*;
 
 pub(super) async fn handler(callback: Callback<u8>) {
-    let mut idx = use_signal(|| 0);
+    let mut idx = use_signal(|| 0u8);
 
     let arguments = idx.with_mut(|w| {
-        *w = if *w as u16 + 1 > u8::MAX as u16 {
-            0
-        } else {
-            *w + 1
-        };
+        let inc_or_zero = if let Some(x) = w.checked_add(1) { x } else { 0 };
 
-        *w
+        *w = inc_or_zero;
+        inc_or_zero
     });
 
     callback.call(arguments)
