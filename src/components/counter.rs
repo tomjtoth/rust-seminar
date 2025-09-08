e11507581a0ba06e0a20303e9c6e68081ed0e4e0
Tamás Tóth <tomjtoth@gmail.com>
2025-09-08 20:05:12 +0300

    protect against overflow in simple counter

--- src/components/counter.rs
+++ src/components/counter.rs
@@ -2,15 +2,18 @@ use dioxus::prelude::*;
 
 #[component]
 pub fn Counter() -> Element {
-    let mut counter = use_signal(|| 0);
+    let mut counter = use_signal(|| 0u8);
 
     rsx! {
         "current count: "
         button {
             class: "w-fit",
             title: "increment by one",
-            onclick: move |_evt| {
-                counter.with_mut(|w| *w += 1)
+            onclick: move |_| {
+                counter.with_mut(|w| {
+                    let inc_or_zero = if let Some(x) = w.checked_add(1) { x } else { 0 };
+                    *w = inc_or_zero;
+                })
             },
             "{counter}++"
         }
