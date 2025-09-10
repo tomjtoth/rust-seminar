7690a47b1d1660088d4fbed576c746c37a23e715
Tamás Tóth <tomjtoth@gmail.com>
2025-09-10 21:32:09 +0300

    swapped parent colors

--- src/components/context_provider/mod.rs
+++ src/components/context_provider/mod.rs
@@ -9,12 +9,11 @@ use parent::Parent;
 #[component]
 pub fn ContextProvider() -> Element {
     rsx! {
-        Parent { bg: "bg-red-200 dark:bg-red-800",
+        Parent { bg: "bg-orange-200 dark:bg-orange-800",
             Child { bg: "bg-purple-200 dark:bg-purple-800" }
             Child { bg: "bg-green-200 dark:bg-green-800" }
         }
-
-        Parent { bg: "bg-orange-200 dark:bg-orange-800",
+        Parent { bg: "bg-red-200 dark:bg-red-800",
             Child { bg: "bg-blue-200 dark:bg-blue-800" }
             Child { bg: "bg-yellow-200 dark:bg-yellow-800" }
         }
