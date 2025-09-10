8765f995aec1be59f38eb787149473c16dd7ec90
Tamás Tóth <tomjtoth@gmail.com>
2025-09-10 21:11:33 +0300

    added shades for the dark mode in context_rpovider

--- src/components/context_provider/mod.rs
+++ src/components/context_provider/mod.rs
@@ -9,14 +9,14 @@ use parent::Parent;
 #[component]
 pub fn ContextProvider() -> Element {
     rsx! {
-        Parent { bg: "bg-red-200",
-            Child { bg: "bg-purple-200" }
-            Child { bg: "bg-green-200" }
+        Parent { bg: "bg-red-200 dark:bg-red-800",
+            Child { bg: "bg-purple-200 dark:bg-purple-800" }
+            Child { bg: "bg-green-200 dark:bg-green-800" }
         }
 
-        Parent { bg: "bg-gray-200",
-            Child { bg: "bg-blue-200" }
-            Child { bg: "bg-yellow-200" }
+        Parent { bg: "bg-orange-200 dark:bg-orange-800",
+            Child { bg: "bg-blue-200 dark:bg-blue-800" }
+            Child { bg: "bg-yellow-200 dark:bg-yellow-800" }
         }
     }
 }
