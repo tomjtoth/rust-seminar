2924872cadf95a5ad839ae2b34e0e155094e06b4
Tamás Tóth <tomjtoth@gmail.com>
2025-09-10 15:39:07 +0300

    added +1 button to the fullstack demo for the fancy nullifier

--- src/components/fullstack/mod.rs
+++ src/components/fullstack/mod.rs
@@ -2,11 +2,19 @@ use dioxus::prelude::*;
 
 use crate::{
     components::global_signal::{buttons::Incrementer, model::COUNTER},
-    services::get_0i8_from_server,
+    services::{get_0i8_from_server, get_fancy_struct_from_server, FancyStruct},
 };
 
 #[component]
 pub fn QueryServer() -> Element {
+    let fancy_handler = |_| async {
+        if let Ok(fs) = get_fancy_struct_from_server().await {
+            let FancyStruct { inner: (_idc, arr) } = fs;
+
+            COUNTER.with_mut(|mutable| *mutable = *arr.last().unwrap());
+        }
+    };
+
     rsx! {
         p {
             "This manipulates the same global signal as seen previously.."
@@ -23,7 +31,12 @@ pub fn QueryServer() -> Element {
                 }
             },
 
-            "null counter (async via server route)"
+            "null counter (via plain server fn)"
+        }
+
+        button {
+            onclick: fancy_handler,
+            "null counter (via fancy server fn)"
         }
     }
 }
