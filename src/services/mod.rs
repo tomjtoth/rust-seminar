3a924f3a4d322e765b90ef2dcbb1b5ed077c1a0f
Tamás Tóth <tomjtoth@gmail.com>
2025-09-10 15:38:02 +0300

    impl fancy_server_fn for fullstack demo

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,6 +1,19 @@
 use dioxus::prelude::*;
+use serde::{Deserialize, Serialize};
 
 #[server]
 pub async fn get_0i8_from_server() -> Result<i8, ServerFnError> {
     Ok(0)
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct FancyStruct {
+    pub inner: (u8, Vec<i8>),
+}
+
+#[server]
+pub async fn get_fancy_struct_from_server() -> Result<FancyStruct, ServerFnError> {
+    Ok(FancyStruct {
+        inner: (123, vec![2, -3, 5, 0]),
+    })
 }
