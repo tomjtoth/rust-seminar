98a155feb2faa822bb1f26e6ea39275a479aab09
Tamás Tóth <tomjtoth@gmail.com>
2025-09-12 17:09:49 +0300

    impl get_string_from_server(optional_delay)

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -17,3 +17,12 @@ pub async fn get_fancy_struct_from_server() -> Result<FancyStruct, ServerFnError
         inner: (123, vec![2, -3, 5, 0]),
     })
 }
+
+#[server]
+pub async fn get_string_from_server(delay: Option<u32>) -> Result<String, ServerFnError> {
+    if let Some(millis) = delay {
+        crate::utils::std_sleep(millis);
+    }
+
+    Ok("string from server".to_string())
+}
