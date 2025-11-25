064c404c16875144003ea3bcab3f64d452835f76
Tamás Tóth <tomjtoth@gmail.com>
2025-11-25 17:35:46 +0200

    added next.js app for reference

---
+++ next/README.md
@@ -0,0 +1,7 @@
+# Next.js
+
+This is the reference for dioxus and egui, launch it with:
+
+```sh
+npm run dev
+```
---
+++ next/app/_components/navbar.tsx
@@ -0,0 +1,22 @@
+import Link from "next/link";
+
+const RE_REPLACER = /\W/g;
+
+export default function NavBar() {
+  return (
+    <ul className="p-2 flex flex-wrap gap-2 *:border *:rounded **:p-2 *:text-nowrap">
+      {[
+        "counters",
+        "controlled-input",
+        "context-providers",
+        "global-state",
+        "callbacks",
+        "fullstack",
+      ].map((href) => (
+        <li key={href}>
+          <Link {...{ href }}>{href.replaceAll(RE_REPLACER, " ")}</Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
