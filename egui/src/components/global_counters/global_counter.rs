b6cc4827547e450bee37f3d583a09cbac7004983
Tamás Tóth <tomjtoth@gmail.com>
2025-11-26 22:05:38 +0200

    renamed increment_by in egui/global_counters

--- egui/src/components/global_counters/global_counter.rs
+++ egui/src/components/global_counters/global_counter.rs
@@ -2,20 +2,20 @@ use eframe::egui::Ui;
 
 use crate::MyEguiApp;
 
-pub fn global_counter(ui: &mut Ui, state: &mut MyEguiApp, diff: i8) {
+pub fn global_counter(ui: &mut Ui, state: &mut MyEguiApp, increment_by: i8) {
     if ui
-        .button(if diff == 0 {
+        .button(if increment_by == 0 {
             "null it".to_string()
         } else {
-            let op = if diff > 0 { "+" } else { "-" };
-            format!("{} {} {}", state.global_counter, op, diff.abs())
+            let op = if increment_by > 0 { "+" } else { "-" };
+            format!("{} {} {}", state.global_counter, op, increment_by.abs())
         })
         .clicked()
     {
-        state.global_counter = if diff == 0 {
+        state.global_counter = if increment_by == 0 {
             0
         } else {
-            state.global_counter.checked_add(diff).unwrap_or(0)
+            state.global_counter.checked_add(increment_by).unwrap_or(0)
         }
     }
 }
